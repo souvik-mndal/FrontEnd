31d448519cfbfca9edde6109b995e184d78a03ee
souvik mndal <souvkmndal@gmail.com>
2025-06-03 00:26:09 +0530

    drag box

--- 03.JS/04.DragBox/script.js
+++ 03.JS/04.DragBox/script.js
@@ -1,24 +1,24 @@
 const box1 = document.querySelector("#box1");
 const box2 = document.querySelector("#box2");
-let child = document.querySelectorAll(".element");
-for( let ele of child ){
+let elements = document.querySelectorAll(".element");
+for( let ele of elements ){
     ele.addEventListener("dragstart",(e)=>{
-        let item = e.target;
+        let child = e.target;
         box2.addEventListener("dragover",(e)=>{
             e.preventDefault();
             e.stopPropagation();
         })
         box2.addEventListener("drop",()=>{
-            box2.append(item);
-            item = "";
+            box2.append(child);
+            child = "";
         })
         box1.addEventListener("dragover",(e)=>{
             e.preventDefault();
             e.stopPropagation();
         })
         box1.addEventListener("drop",()=>{
-            box1.append(item);
-            item = "";
+            box1.append(child);
+            child = "";
         })
     })
 }
