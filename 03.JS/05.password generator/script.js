bd479b12d0abab9334a556b731298431b705c676
souvik mndal <souvkmndal@gmail.com>
2025-06-03 00:44:59 +0530

    password generator

--- 03.JS/05.password generator/script.js
+++ 03.JS/05.password generator/script.js
@@ -13,52 +13,49 @@ range.addEventListener("input",()=>{
 let cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let sml = "abcdefghijklmnopqrstuvwxyz";
 let num = "0123456789";
-let sym = "!@#$%^&*()_+-=[]{}|;:',.<>?/";
-
+let sym = "~!@#$%^&*()_-+*/<,>.:;{}[]"
 let all = "";
 let ans = "";
-
 function rndm(){
-    let ele = Math.floor((Math.random()*all.length));
-    return ele;
+    return Math.floor(Math.random()*all.length);
 }
-generate.addEventListener('click',()=>{
-    if( capital.checked === true ){
-        all+=cap;
+
+generate.addEventListener("click",()=>{
+    
+    if(capital.checked === true ){
+        all += cap;
         console.log(all);
     }
-    if( lower.checked === true ){
-        all+=sml;
+    if(lower.checked === true ){
+        all += sml;
         console.log(all);
     }
-    if( number.checked === true ){
-        all+=num;
+    if(number.checked === true ){
+        all += num;
         console.log(all);
     }
-    if( symbol.checked === true ){
-        all+=sym;
+    if(symbol.checked === true ){
+        all += sym;
         console.log(all);
     }
-    if(all === "" ){
-        alert("Please select atleast one field");
+    if( all === "" ){
+        alert("Choose any one of the options");
     }
     for( let i=0 ; i<range.value ; i++ ){
-        let val = rndm();
-        ans += all.charAt(val);
-        console.log(all.charAt(val));
+        ans += all.charAt(rndm());
     }
-    document.querySelector("h3").innerText = ans ;
+    document.querySelector("h3").innerText = ans;
     ans = "";
     all = "";
+
 })
 copy.addEventListener("click",()=>{
-    let cp = document.querySelector("h3").innerText
-    if( cp === ""){
-        alert("Empty Value");
+    let h = document.querySelector("h3");
+    if( h.innerText === "" ){
+        alert("No charecter found");
     }
-    else{
-        alert("Copied to Clipboard");
+    window.navigator.clipboard.writeText(h.innerText);
+    if( h.innerText !== "" ){
+        alert("Copied to the Clipboard");
     }
-    window.navigator.clipboard.writeText(cp)
-    
 })
