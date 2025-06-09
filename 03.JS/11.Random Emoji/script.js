86617fca0d6191545ec8466bb7cc63db0e4b9487
souvik mndal <souvkmndal@gmail.com>
2025-06-10 01:52:41 +0530

    Random Emoji Generator

--- 03.JS/11.Random Emoji/script.js
+++ 03.JS/11.Random Emoji/script.js
@@ -2,4 +2,22 @@
 
 // https://emoji-api.com/emojis?access_key=5fc5d0fb93231f009fa33fc540190d94f1b07790
 
+let ACCESS_KEY = "5fc5d0fb93231f009fa33fc540190d94f1b07790";
 
+let ji = document.querySelector("#emoji");
+
+function rndm(len){
+    return Math.floor(Math.random()*len)
+}
+
+async function emo(){
+    const req = await fetch(`https://emoji-api.com/emojis?access_key=${ACCESS_KEY}`)
+    const res = await req.json();
+    let r = rndm(res.length)
+    ji.innerText = res[r].character;
+}
+
+let btn =  document.querySelector("#gen");
+btn.addEventListener("click",()=>{
+    emo();
+})
