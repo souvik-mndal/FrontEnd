c4ac793f7285826faf0307a04c75115763a767a1
souvik mndal <souvkmndal@gmail.com>
2025-06-11 04:41:42 +0530

    drag box

--- 03.JS/04.DragBox/script.js
+++ 03.JS/04.DragBox/script.js
@@ -1,24 +1,52 @@
+// const box1 = document.querySelector("#box1");
+// const box2 = document.querySelector("#box2");
+// let elements = document.querySelectorAll(".element");
+// for( let ele of elements ){
+//     ele.addEventListener("dragstart",(e)=>{
+//         let child = e.target;
+//         box2.addEventListener("dragover",(e)=>{
+//             e.preventDefault();
+//             e.stopPropagation();
+//         })
+//         box2.addEventListener("drop",()=>{
+//             box2.append(child);
+//             child = "";
+//         })
+//         box1.addEventListener("dragover",(e)=>{
+//             e.preventDefault();
+//             e.stopPropagation();
+//         })
+//         box1.addEventListener("drop",()=>{
+//             box1.append(child);
+//             child = "";
+//         })
+//     })
+// }
+
+
 const box1 = document.querySelector("#box1");
 const box2 = document.querySelector("#box2");
-let elements = document.querySelectorAll(".element");
-for( let ele of elements ){
-    ele.addEventListener("dragstart",(e)=>{
-        let child = e.target;
+let ele = document.querySelectorAll(".element");
+
+for( let el of ele ){
+    el.addEventListener("dragstart",(e)=>{
+        let item = e.target;
+        // console.log(item);
         box2.addEventListener("dragover",(e)=>{
             e.preventDefault();
             e.stopPropagation();
         })
         box2.addEventListener("drop",()=>{
-            box2.append(child);
-            child = "";
+            box2.append(item);
+            item = "";
         })
         box1.addEventListener("dragover",(e)=>{
             e.preventDefault();
             e.stopPropagation();
         })
         box1.addEventListener("drop",()=>{
-            box1.append(child);
-            child = "";
+            box1.append(item);
+            item = "";
         })
     })
 }
