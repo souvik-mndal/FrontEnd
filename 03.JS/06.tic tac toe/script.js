0c8d4f82a32ef2bdc075585e237175ff7030e894
souvik mndal <souvkmndal@gmail.com>
2025-06-11 17:56:44 +0530

    tic tac toe

--- 03.JS/06.tic tac toe/script.js
+++ 03.JS/06.tic tac toe/script.js
@@ -1,23 +1,29 @@
+
+
+
+
 const grid = document.querySelector("#grid-cont");
-let child = document.querySelectorAll(".element");
-let h = document.querySelector("#h3");
-// console.log(child);
+const ele = document.querySelectorAll(".element");
+const h3 = document.querySelector("#h3");
+const restart = document.querySelector("#res");
+
+
 let current = "X";
 
 let count = 0;
 
-function start(e){
-    if( e.target.id !== "grid-cont"){
+function add(e){
+    if( e.target.id !== "grid-cont" ){
         if( e.target.innerText === "" ){
             e.target.innerText = current;
-            count++;
             console.log(count);
+            count++;
             check();
-            if( count === 9 && h.innerText === ""){
-                h.innerText = "Match is Draw";
+            if( count === 9 && h3.innerText === "" ){
+                h3.innerText = "The match is draw";
             }
             if( current === "X" ){
-                current = "O" ;
+                current = "O";
             }
             else{
                 current = "X";
@@ -25,44 +31,34 @@ function start(e){
         }
     }
 }
+grid.addEventListener("click", add)
 
+let logic = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
-grid.addEventListener("click", start)
-
-let condition = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 function check(){
-    for( let ele of condition ){
-        let b0 = ele[0];
-        let b1 = ele[1];
-        let b2 = ele[2];
-        let i0 = child[b0].innerText;
-        let i1 = child[b1].innerText;
-        let i2 = child[b2].innerText;
-        let c0 = child[b0];
-        let c1 = child[b1];
-        let c2 = child[b2];
-        // console.log(i0 , i1 , i2);
-        if( i0 !== "" && i1 !== "" && i2 !== "" ){
-            if( i0 === i1 && i1 === i2){
-                c0.classList.add("winner");
-                c1.classList.add("winner");
-                c2.classList.add("winner");
-                h.innerText = `The Winner is ${i0}`
-                grid.removeEventListener("click",start);
+    for( let l of logic ){
+        let one = ele[l[0]].innerText;
+        let two = ele[l[1]].innerText;
+        let three = ele[l[2]].innerText;
+        if( one !== "" && two !== "" && three !== "" ){
+            if( one === two && two === three){
+                console.log(current);
+                h3.innerText = `The Winner is ${current}`;
+                grid.removeEventListener("click",add);
             }
         }
     }
+
 }
-
-let btn = document.querySelector("#res");
-btn.addEventListener("dblclick",()=>{
-    for( let ement of child ){
-        ement.innerText = "";
-        ement.classList.remove("winner");
-    }
-    h.innerText = "";
-grid.addEventListener("click", start)
-current = "X";
-count = 0;
-
+restart.addEventListener("click",()=>{
+    for( let l of logic ){
+         ele[l[0]].innerText = "";
+         ele[l[1]].innerText = "";
+         ele[l[2]].innerText = "";
+         
+        }
+        current = "X";
+        h3.innerText = "";
+        count = 0;
+        grid.addEventListener("click", add)
 })
