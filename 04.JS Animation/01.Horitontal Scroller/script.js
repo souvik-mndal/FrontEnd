3cc212c0e99a4fbfaf76514308397cdf4d14cfc8
souvik mndal <souvkmndal@gmail.com>
2025-06-15 10:46:29 +0530

    Horizontal Scroller

---
+++ 04.JS Animation/01.Horitontal Scroller/script.js
@@ -0,0 +1,12 @@
+gsap.to("#page2 h1",{
+    x: "-180%",
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        markers:true,
+        start:"top 0",
+        end:"top -100%",
+        pin:true,
+        scrub:1
+    }
+})
