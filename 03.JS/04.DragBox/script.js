ef037fcc99a4ca2943ecf759721a0b78c56d08f4
souvik mndal <souvkmndal@gmail.com>
2025-07-16 00:27:18 +0530

    drag drop

--- 03.JS/04.DragBox/script.js
+++ 03.JS/04.DragBox/script.js
@@ -24,29 +24,53 @@
 // }
 
 
-const box1 = document.querySelector("#box1");
-const box2 = document.querySelector("#box2");
-let ele = document.querySelectorAll(".element");
+// const box1 = document.querySelector("#box1");
+// const box2 = document.querySelector("#box2");
+// let ele = document.querySelectorAll(".element");
 
-for( let el of ele ){
-    el.addEventListener("dragstart",(e)=>{
-        let item = e.target;
-        // console.log(item);
-        box2.addEventListener("dragover",(e)=>{
+// for( let el of ele ){
+//     el.addEventListener("dragstart",(e)=>{
+//         let item = e.target;
+//         // console.log(item);
+//         box2.addEventListener("dragover",(e)=>{
+//             e.preventDefault();
+//             e.stopPropagation();
+//         })
+//         box2.addEventListener("drop",()=>{
+//             box2.append(item);
+//             item = "";
+//         })
+//         box1.addEventListener("dragover",(e)=>{
+//             e.preventDefault();
+//             e.stopPropagation();
+//         })
+//         box1.addEventListener("drop",()=>{
+//             box1.append(item);
+//             item = "";
+//         })
+//     })
+// }
+
+
+const box1 = document.querySelector("#box1")
+const box2 = document.querySelector("#box2")
+const boxes = document.querySelectorAll(".element")
+boxes.forEach((items) => {
+    items.addEventListener("dragstart",(e) => {
+        let i = e.target;
+        box2.addEventListener("dragover",(e) => {
             e.preventDefault();
-            e.stopPropagation();
         })
-        box2.addEventListener("drop",()=>{
-            box2.append(item);
-            item = "";
+        box2.addEventListener("drop",() => {
+            box2.append(i);
+            i = "";
         })
-        box1.addEventListener("dragover",(e)=>{
+        box1.addEventListener("dragover",(e) => {
             e.preventDefault();
-            e.stopPropagation();
         })
-        box1.addEventListener("drop",()=>{
-            box1.append(item);
-            item = "";
+        box1.addEventListener("drop",() => {
+            box1.append(i);
+            i = "";
         })
     })
-}
+})
