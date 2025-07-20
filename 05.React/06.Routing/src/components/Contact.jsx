ee93bdeec629b981c27c2daec423afa0758f48ab
souvik mndal <souvkmndal@gmail.com>
2025-07-20 17:41:30 +0530

    React

--- 05.React/06.Routing/src/components/Contact.jsx
+++
@@ -1,7 +0,0 @@
-export function Contact(){
-    return (
-        <div className="h-[90%] px-24 pt-10 text-5xl flex justify-center items-center">
-            <h1>Contact</h1>
-        </div>
-    )
-}
--- 05.React/06.Routing/src/components/Project.jsx
+++
@@ -1,7 +0,0 @@
-export function Project(){
-    return (
-        <div className="h-[90%] px-24 pt-10 text-5xl flex justify-center items-center">
-            <h1>Projects</h1>
-        </div>
-    )
-}
