327e3d7671f88ad39f24ab3d9ee4c5bffb74ab16
souvik mndal <souvkmndal@gmail.com>
2025-07-21 16:43:29 +0530

    Product showcase in React

---
+++ 05.React/02.Product Showcase/src/Components/Products.jsx
@@ -0,0 +1,319 @@
+
+
+let data = [
+    {
+      "id": 1,
+      "products": [
+        {
+          "id": 168,
+          "title": "Charger SXT RWD",
+          "price": 32999.99,
+          "quantity": 3,
+          "total": 98999.97,
+          "discountPercentage": 13.39,
+          "discountedTotal": 85743.87,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/vehicle/Charger%20SXT%20RWD/thumbnail.png"
+        },
+        {
+          "id": 78,
+          "title": "Apple MacBook Pro 14 Inch Space Grey",
+          "price": 1999.99,
+          "quantity": 2,
+          "total": 3999.98,
+          "discountPercentage": 18.52,
+          "discountedTotal": 3259.18,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/laptops/Apple%20MacBook%20Pro%2014%20Inch%20Space%20Grey/thumbnail.png"
+        },
+        {
+          "id": 183,
+          "title": "Green Oval Earring",
+          "price": 24.99,
+          "quantity": 5,
+          "total": 124.95,
+          "discountPercentage": 6.28,
+          "discountedTotal": 117.1,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/womens-jewellery/Green%20Oval%20Earring/thumbnail.png"
+        },
+        {
+          "id": 100,
+          "title": "Apple Airpods",
+          "price": 129.99,
+          "quantity": 5,
+          "total": 649.95,
+          "discountPercentage": 12.84,
+          "discountedTotal": 566.5,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Airpods/thumbnail.png"
+        }
+      ],
+      "total": 103774.85,
+      "discountedTotal": 89686.65,
+      "userId": 33,
+      "totalProducts": 4,
+      "totalQuantity": 15
+    },
+    {
+      "id": 2,
+      "products": [
+        {
+          "id": 144,
+          "title": "Cricket Helmet",
+          "price": 44.99,
+          "quantity": 4,
+          "total": 179.96,
+          "discountPercentage": 11.47,
+          "discountedTotal": 159.32,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/sports-accessories/Cricket%20Helmet/thumbnail.png"
+        },
+        {
+          "id": 124,
+          "title": "iPhone X",
+          "price": 899.99,
+          "quantity": 4,
+          "total": 3599.96,
+          "discountPercentage": 8.03,
+          "discountedTotal": 3310.88,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/smartphones/iPhone%20X/thumbnail.png"
+        },
+        {
+          "id": 148,
+          "title": "Golf Ball",
+          "price": 9.99,
+          "quantity": 4,
+          "total": 39.96,
+          "discountPercentage": 11.24,
+          "discountedTotal": 35.47,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/sports-accessories/Golf%20Ball/thumbnail.png"
+        },
+        {
+          "id": 122,
+          "title": "iPhone 6",
+          "price": 299.99,
+          "quantity": 3,
+          "total": 899.97,
+          "discountPercentage": 19.64,
+          "discountedTotal": 723.22,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/smartphones/iPhone%206/thumbnail.png"
+        },
+        {
+          "id": 110,
+          "title": "Selfie Lamp with iPhone",
+          "price": 14.99,
+          "quantity": 5,
+          "total": 74.95,
+          "discountPercentage": 19.87,
+          "discountedTotal": 60.06,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/mobile-accessories/Selfie%20Lamp%20with%20iPhone/thumbnail.png"
+        }
+      ],
+      "total": 4794.8,
+      "discountedTotal": 4288.95,
+      "userId": 142,
+      "totalProducts": 5,
+      "totalQuantity": 20
+    },
+    {
+      "id": 3,
+      "products": [
+        {
+          "id": 98,
+          "title": "Rolex Submariner Watch",
+          "price": 13999.99,
+          "quantity": 1,
+          "total": 13999.99,
+          "discountPercentage": 16.35,
+          "discountedTotal": 11710.99,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/mens-watches/Rolex%20Submariner%20Watch/thumbnail.png"
+        },
+        {
+          "id": 125,
+          "title": "Oppo A57",
+          "price": 249.99,
+          "quantity": 5,
+          "total": 1249.95,
+          "discountPercentage": 16.54,
+          "discountedTotal": 1043.21,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/smartphones/Oppo%20A57/thumbnail.png"
+        },
+        {
+          "id": 55,
+          "title": "Egg Slicer",
+          "price": 6.99,
+          "quantity": 2,
+          "total": 13.98,
+          "discountPercentage": 16.04,
+          "discountedTotal": 11.74,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/kitchen-accessories/Egg%20Slicer/thumbnail.png"
+        },
+        {
+          "id": 62,
+          "title": "Ice Cube Tray",
+          "price": 5.99,
+          "quantity": 2,
+          "total": 11.98,
+          "discountPercentage": 8.25,
+          "discountedTotal": 10.99,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/kitchen-accessories/Ice%20Cube%20Tray/thumbnail.png"
+        },
+        {
+          "id": 132,
+          "title": "Samsung Galaxy S8",
+          "price": 499.99,
+          "quantity": 3,
+          "total": 1499.97,
+          "discountPercentage": 8.84,
+          "discountedTotal": 1367.37,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/smartphones/Samsung%20Galaxy%20S8/thumbnail.png"
+        }
+      ],
+      "total": 16775.87,
+      "discountedTotal": 14144.3,
+      "userId": 108,
+      "totalProducts": 5,
+      "totalQuantity": 13
+    },
+    {
+      "id": 4,
+      "products": [
+        {
+          "id": 187,
+          "title": "Golden Shoes Woman",
+          "price": 49.99,
+          "quantity": 5,
+          "total": 249.95,
+          "discountPercentage": 1.64,
+          "discountedTotal": 245.85,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/womens-shoes/Golden%20Shoes%20Woman/thumbnail.png"
+        },
+        {
+          "id": 40,
+          "title": "Strawberry",
+          "price": 3.99,
+          "quantity": 5,
+          "total": 19.95,
+          "discountPercentage": 4.6,
+          "discountedTotal": 19.03,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Strawberry/thumbnail.png"
+        },
+        {
+          "id": 156,
+          "title": "Green and Black Glasses",
+          "price": 34.99,
+          "quantity": 5,
+          "total": 174.95,
+          "discountPercentage": 4.34,
+          "discountedTotal": 167.36,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/sunglasses/Green%20and%20Black%20Glasses/thumbnail.png"
+        },
+        {
+          "id": 62,
+          "title": "Ice Cube Tray",
+          "price": 5.99,
+          "quantity": 2,
+          "total": 11.98,
+          "discountPercentage": 8.25,
+          "discountedTotal": 10.99,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/kitchen-accessories/Ice%20Cube%20Tray/thumbnail.png"
+        }
+      ],
+      "total": 456.83,
+      "discountedTotal": 443.23,
+      "userId": 87,
+      "totalProducts": 4,
+      "totalQuantity": 17
+    },
+    {
+      "id": 5,
+      "products": [
+        {
+          "id": 108,
+          "title": "iPhone 12 Silicone Case with MagSafe Plum",
+          "price": 29.99,
+          "quantity": 2,
+          "total": 59.98,
+          "discountPercentage": 14.68,
+          "discountedTotal": 51.17,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/mobile-accessories/iPhone%2012%20Silicone%20Case%20with%20MagSafe%20Plum/thumbnail.png"
+        },
+        {
+          "id": 138,
+          "title": "Baseball Ball",
+          "price": 8.99,
+          "quantity": 5,
+          "total": 44.95,
+          "discountPercentage": 18.49,
+          "discountedTotal": 36.64,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/sports-accessories/Baseball%20Ball/thumbnail.png"
+        },
+        {
+          "id": 157,
+          "title": "Party Glasses",
+          "price": 19.99,
+          "quantity": 2,
+          "total": 39.98,
+          "discountPercentage": 19.17,
+          "discountedTotal": 32.32,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/sunglasses/Party%20Glasses/thumbnail.png"
+        },
+        {
+          "id": 8,
+          "title": "Dior J'adore",
+          "price": 89.99,
+          "quantity": 3,
+          "total": 269.97,
+          "discountPercentage": 10.79,
+          "discountedTotal": 240.84,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/thumbnail.png"
+        },
+        {
+          "id": 80,
+          "title": "Huawei Matebook X Pro",
+          "price": 1399.99,
+          "quantity": 5,
+          "total": 6999.95,
+          "discountPercentage": 9.99,
+          "discountedTotal": 6300.65,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/laptops/Huawei%20Matebook%20X%20Pro/thumbnail.png"
+        },
+        {
+          "id": 28,
+          "title": "Ice Cream",
+          "price": 5.49,
+          "quantity": 3,
+          "total": 16.47,
+          "discountPercentage": 10,
+          "discountedTotal": 14.82,
+          "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Ice%20Cream/thumbnail.png"
+        }
+      ],
+      "total": 7431.3,
+      "discountedTotal": 6676.44,
+      "userId": 134,
+      "totalProducts": 6,
+      "totalQuantity": 20
+    }]
+
+
+
+export function Products(){
+    return (
+        <>
+            {
+                data.map((item)=>{
+                    return item.products.map((innerItem)=>{
+                        return <div className="product-card">
+                            <img
+                                src={innerItem.thumbnail}
+                                alt="Product"
+                                className="product-image"
+                            />
+                            <div className="product-content">
+                                <div className="product-caption">{innerItem.title}</div>
+                                <div className="product-price">{innerItem.price}</div>
+                            </div>
+                        </div>
+                    })
+                })
+            }
+        </>
+    )
+}
