aca1be4ca6cae1f3550ac166d9f403437478c486
souvik mndal <souvkmndal@gmail.com>
2025-07-23 17:59:18 +0530

    Form handling Controlled verse

---
+++ 05.React/05.Form Handling Controlled/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [email,setEmail] = useState("")
+  let [pass,setPass] = useState("")
+
+  return (
+    <>
+      <input onChange={(e)=>{
+        setEmail(e.target.value)
+      }} type="email" name="email" id="email" placeholder='janedoe@gmail.com' value={email}/>
+
+      <br /><br />
+
+      <input onChange={(e)=>{
+        setPass(e.target.value)
+      }} type="password" name="password" id="password" placeholder='password' value={pass}/>
+
+      <br /><br />
+
+      <button onClick={(e)=>{
+        e.preventDefault()
+        console.log({email,password:pass});
+      }}>Submit</button>
+    </>
+  )
+}
+
+export default App
